7a5efac02323664099793eafd427581eddd528d1
jomozen <tylerde13@gmail.com>
2023-07-04 17:21:02 -0400

    Update

---
+++ pong.py
@@ -0,0 +1,102 @@
+import pygame, sys , random
+
+def ballsack():
+    global ball_speed_x, ball_speed_y
+
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+
+    if ball.top <= 0 or ball.bottom >= screen_height:
+        ball_speed_y *= -1
+    if ball.left <= 0 or ball.right >= screen_width:
+        ballreset()
+    #visuals
+    if ball.colliderect(player) or ball.colliderect(opponent):
+        ball_speed_x *= -1
+
+# This function updates the position of the player based on the player_speed variable
+def update_player():
+    # Increment the player's y-coordinate by the player_speed variable
+    player.y += player_speed
+
+def player_animation():
+    update_player()
+    if player.top <= 0:
+        player.top = 0
+    if player.bottom >= screen_height:
+        player.bottom = screen_height
+    
+def opponent_ai():
+    if opponent.top < ball.y:
+        opponent.top += opponent_speed
+    if opponent.bottom > ball.y:
+        opponent.bottom -= opponent_speed
+    if opponent.top <= 0:
+        opponent.top = 0
+    if opponent.bottom >= screen_height:
+        opponent.bottom = screen_height
+    
+#input of game files
+pygame.init()
+clock = pygame.time.Clock()
+def ballreset():
+    global ball_speed_y, ball_speed_x
+    ball.center = (screen_width/2, screen_height/2)
+    ball_speed_x *= random.choice((1,-1))
+    ball_speed_y *= random.choice((1,-1))
+
+
+#display setting
+screen_width = 1280
+screen_height = 960
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Pong')
+
+
+ball = pygame.Rect(screen_width/2, screen_height/2, 30,30)
+player = pygame.Rect(screen_width - 20,screen_height/2 - 70, 10,140)
+opponent = pygame.Rect(10,screen_height/2 - 70, 10,140)
+
+ball_speed_x = 7 * random.choice((1,-1))
+ball_speed_y = 7 * random.choice((1,-1))
+player_speed = 0
+opponent_speed = 8
+
+bg_color = pygame.Color('grey12')
+light_grey = (200,200,200)
+
+#gandling the user input
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                player_speed += 7
+            if event.key == pygame.K_UP:
+                player_speed -= 7
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_DOWN:
+                player_speed -= 7
+            if event.key == pygame.K_UP:
+                player_speed += 7
+
+    player_animation()
+    ballsack()
+    opponent_ai()
+
+    screen.fill(bg_color)
+    pygame.draw.rect(screen,light_grey,player)
+    pygame.draw.rect(screen,light_grey,opponent)
+    pygame.draw.ellipse(screen,light_grey,ball)
+    #pygame.draw.aaline(screen, light_grey,(screen_width/2), (screen_width/2,screen_height))
+    #pygame.draw.aaline(screen, light_grey,(screen_width/2), screen_height)
+    pygame.draw.aaline(screen, light_grey, (screen_width/2, screen_height), (screen_width/2, 0))
+
+
+
+
+    #updatng the window
+    pygame.display.flip()
+    clock.tick(60)
