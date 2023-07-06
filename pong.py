a458cf3fc97e43dbeda450304e3b8ba3917ee7c1
jomozen <tylerde13@gmail.com>
2023-07-06 15:57:50 -0400

    Updated score and timer

--- pong.py
+++ pong.py
@@ -2,14 +2,19 @@ import pygame, sys , random
 
 def ballsack():
     global ball_speed_x, ball_speed_y
-
+    global player_score, opponent_score, score_time
     ball.x += ball_speed_x
     ball.y += ball_speed_y
 
     if ball.top <= 0 or ball.bottom >= screen_height:
         ball_speed_y *= -1
+    if ball.left <= 0:
+        player_score += 1
+    if ball.right >= screen_width:
+        opponent_score += 1  
     if ball.left <= 0 or ball.right >= screen_width:
-        ballreset()
+        score_time = pygame.time.get_ticks()  
+
     #visuals
     if ball.colliderect(player) or ball.colliderect(opponent):
         ball_speed_x *= -1
@@ -40,10 +45,26 @@ def opponent_ai():
 pygame.init()
 clock = pygame.time.Clock()
 def ballreset():
-    global ball_speed_y, ball_speed_x
+    global ball_speed_y, ball_speed_x, score_time
     ball.center = (screen_width/2, screen_height/2)
-    ball_speed_x *= random.choice((1,-1))
-    ball_speed_y *= random.choice((1,-1))
+    current_time = pygame.time.get_ticks()
+    if current_time - score_time < 700:
+        number_three = game_font.render("3",False, light_grey)
+        screen.blit(number_three,(screen_width/2 - 10, screen_height/2 + 20))
+    if current_time - score_time > 700 and current_time - score_time < 1400:
+        number_two = game_font.render("2",False, light_grey)
+        screen.blit(number_two,(screen_width/2 - 10, screen_height/2 + 20))
+    if current_time - score_time > 1400:
+        number_one = game_font.render("1",False, light_grey)
+        screen.blit(number_one,(screen_width/2 - 10, screen_height/2 + 20))
+
+    if current_time - score_time < 2100:
+        ball_speed_x, ball_speed_y = 0,0
+    else: 
+        ball_speed_x = 7 * random.choice((1,-1))
+        ball_speed_y = 7 * random.choice((1,-1))
+        score_time = None
+
 
 
 #display setting
@@ -52,6 +73,12 @@ screen_height = 960
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Pong')
 
+#text variables
+player_score = 0
+opponent_score = 0
+game_font = pygame.font.Font("freesansbold.ttf", 32)
+
+score_time = None
 
 ball = pygame.Rect(screen_width/2, screen_height/2, 30,30)
 player = pygame.Rect(screen_width - 20,screen_height/2 - 70, 10,140)
@@ -93,6 +120,16 @@ while True:
     #pygame.draw.aaline(screen, light_grey,(screen_width/2), (screen_width/2,screen_height))
     #pygame.draw.aaline(screen, light_grey,(screen_width/2), screen_height)
     pygame.draw.aaline(screen, light_grey, (screen_width/2, screen_height), (screen_width/2, 0))
+
+    if score_time:
+        ballreset()
+
+
+    player_text = game_font.render(f"{player_score}",False , light_grey)
+    opponent_text = game_font.render(f"{opponent_score}",False,light_grey)
+    screen.blit(player_text,(660,470))
+    screen.blit(opponent_text, (600,470))
+
 
 
 
